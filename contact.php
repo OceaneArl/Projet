067493faef377d261b9a08e6c7dcdf99155a3cf8
JohLav <[johana.lavigne@gmail.com](mailto:johana.lavigne@gmail.com>
2023-02-23 08:50:53 +0100

    Modif mardi 22 fév. du CSS formulaire

---
+++ contact.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>SheHacks</title>
+  <link rel="stylesheet" href="contact-style.css" />
+  <!--<link rel="stylesheet" href="style.css">-->
+  <!-- <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+      crossorigin="anonymous"
+    /> -->
+</head>
+
+<body>
+  <?php include "_header.php"; ?>
+  <h2>Nous contacter</h2>
+  <div class="container">
+    <form action="form.php" method="POST">
+      <div class="row">
+        <label for="firstname" class="col-25">Prénom</label>
+        <br>
+        <input type="text" class="col-75" id="firstname" name="firstname" placeholder="Prénom" required />
+      </div>
+      <div class="row">
+        <label for="lastname" class="col-25">Nom</label>
+        <br>
+        <input type="text" class="col-75" id="lastname" name="lastname" placeholder="Nom" required />
+      </div>
+      <div class="row">
+        <label for="email" class="col-25">Email</label>
+        <br>
+        <input type="email" class="col-75" id="email" name="email" size="30" placeholder="exemple@email.com" required />
+      </div>
+      <div class="row">
+        <label for="tel" class="col-25">Téléphone</label>
+        <br>
+        <input type="tel" class="col-75" id="tel" name="tel" pattern="[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}"
+          placeholder="01 23 45 67 89" minlength="10" maxlength="14" required />
+      </div>
+      <div class="row">
+        <label for="subject" class="col-25">Sujet</label>
+        <br>
+        <input type="text" class="col-75" id="subject" name="subject" placeholder="Sujet" required />
+      </div>
+      <div class="row">
+        <label for="message" class="col-25">Message</label>
+        <br>
+        <textarea id="message" class="col-75" name="message" placeholder="Saisir votre message" required></textarea>
+      </div>
+      <br />
+      <div class="row">
+        <div class="row">
+          <input type="submit" value="Envoyer" />
+          <!-- <button class="button is-link has-background-alt">Envoyer</button> -->
+        </div>
+    </form>
+  </div>
+  <!-- <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+      crossorigin="anonymous"
+    ></script> -->
+  <?php include("_footer.php"); ?>
+</body>
+
+</html>
--- form.php
+++ form.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SheHacks</title>
-    <!--<link rel="stylesheet" href="style.css">-->
 
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SheHacks</title>
+	<link rel="stylesheet" href="contact-style.css">
 </head>
 
 <body>
-<?php include("_header.php") ?>
-
-<div class="container">
-	<div class="column is-12">
+	<?php include "_header.php"; ?>
+	<div class="container">
 		<section class="hero is-info is-bold is-small">
-			<div class="hero-body">
-				<div class="container">
-					<h1 class="title">Hello <?php echo $_GET['firstname'] .' '. $_GET['lastname'] . ' from ' . $_GET['campus']; ?> this is your message : </h1>
-					<h2 class="subtitle"> <?php echo $_GET['message']; ?></h2>
-				</div>
-			</div>
+			<h1 class="title">Votre message a bien été envoyé !</h1>
+			<p> Merci
+				<?php echo $_POST['firstname'] . ' ' . $_POST['lastname']; ?> de nous avoir contacté à
+				propos de
+				<?php echo $_POST['subject']; ?>. L'un des membres de notre équipe prendra contact avec
+				vous par email à l'adresse
+				<?php echo $_POST['email']; ?> ou par téléphone au
+				<?php echo $_POST['tel']; ?> dans les meilleurs délais pour traiter votre message.
+				</ br>
+				Pour rappel, voici votre message :
+				<?php echo $_POST['message']; ?>
+			</p>
 		</section>
 	</div>
-</div>
-<?php include("_footer.php"); ?>
+	<?php include("_footer.php"); ?>
 </body>
+
 </html>
